0ac314e08e88f8ea651f995d9b9019bb65436a28
ejanton-sun <81206481+ejanton-sun@users.noreply.github.com>
2021-04-06 11:19:47 +0800

    [SELS-TASK] Can manage user details (#4)
    * [SELS-TASK] Can manage user details
    
    * Update error handling
    
    * Update indentions
    
    Co-authored-by: EJ Anton Sabal Potot <zuming1404@gmail.com>

--- app/Http/Controllers/UserSettingsController.php
+++ app/Http/Controllers/UserSettingsController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Image;
-use Illuminate\Support\Facades\Storage;
+use Hash;
+
 class UserSettingsController extends Controller
 {
     public function index()
@@ -21,9 +22,55 @@ class UserSettingsController extends Controller
             Image::make($avatar)->resize(300, 300)->save(('storage/uploads/avatars/' . $fileName));
             $user = Auth::user();
             $user->avatar = $fileName;
-            $user->save();
+            $user->update();
         }
 
         return $this->index();
+    }
+
+    public function userDetails()
+    {
+        return view('user.details', ["user" => Auth::user()]);
+    }
+
+    public function updateDetails(Request $request)
+    {
+        $user = Auth::user();
+        $request->validate([
+            'email' => 'required|email',
+            'name' => 'required',
+        ]);
+        $user->email =  $request["email"];
+        $user->name =  $request["name"];
+        $user->update();
+
+        return redirect()->route('user.settings');
+    }
+
+    public function changePassword()
+    {
+        return view('user.changePassword', ["user" => Auth::user()]);
+    }
+
+    public function updatePassword(Request $request)
+    {
+        //Check if the Current Password matches with what is in the database.
+        if (!(Hash::check($request->get('current_password'), Auth::user()->password))) {
+            return back()->with('error', 'Your current password does not match with what you provided');
+        }
+        // Compare the Current Password and New Password using[strcmp function]
+        if (strcmp($request->get('current_password'), $request->get('new_password')) == 0) {
+            return back()->with('error', 'Your current password cannot be same with the new password');
+        }
+        //Validate the Password.
+        $request->validate([
+            'current_password' => 'required',
+            'new_password'     => 'required|string|min:6|confirmed'
+        ]);
+        // Save the New Password.
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new_password'));
+        $user->update();
+        return back()->with('message', 'Password changed successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/user/settings', [UserSettingsController::class, 'index'])->name('user.settings');
 Route::put('/user/settings/updateAvatar', [UserSettingsController::class, 'updateAvatar'])->name('user.updateAvatar');
+
+Route::get('/users/settings/changePassword',  [UserSettingsController::class, 'changePassword'])->name('user.changePassword');
+Route::put('/users/settings/updatePassword',  [UserSettingsController::class, 'updatePassword'])->name('user.updatePassword');
+
+Route::get('/users/settings/userDetails',  [UserSettingsController::class, 'userDetails'])->name('user.details');
+Route::put('/user/settings/updateDetails', [UserSettingsController::class, 'updateDetails'])->name('user.updateDetails');
