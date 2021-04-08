22260d449ade90a8ee4e95e9262bc7fb7ecd3956
EJ Anton Sabal Potot <zuming1404@gmail.com>
2021-04-08 18:00:50 +0800

    Updated indention

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="display-4">{{ __('Add Category') }}</h1>
-    <a class="btn btn-primary" href="{{ route('category.index') }}">Back</a>
-    <form class="py-5" action="{{route('category.store')}}" method="post">
-        @csrf
-        <div class="form-floating mb-3">
-            <input
-                type="text"
-                class="form-control"
-                id="title"
-                name="title"
-                placeholder="Basic 100">
-            <label for="title">Title</label>
-        </div>
-        <div class="form-floating">
-            <textarea
-                class="form-control"
-                placeholder="Leave a comment here"
-                name="description"
-                id="description"
-            ></textarea>
-            <label for="description">Description</label>
-        </div>
-        <div class="mt-4">
-            <button type="submit" class="btn btn-primary">Save changes</button>
-        </div>
-    </form>
-</div>
+    <div class="container">
+        <h1 class="display-4">{{ __('Add Category') }}</h1>
+        <a class="btn btn-primary" href="{{ route('category.index') }}">Back</a>
+        <form class="py-5" action="{{route('category.store')}}" method="post">
+            @csrf
+            <div class="form-floating mb-3">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="title"
+                    name="title"
+                    placeholder="Basic 100">
+                <label for="title">Title</label>
+            </div>
+            <div class="form-floating">
+                <textarea
+                    class="form-control"
+                    placeholder="Leave a comment here"
+                    name="description"
+                    id="description"
+                ></textarea>
+                <label for="description">Description</label>
+            </div>
+            <div class="mt-4">
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+        </form>
+    </div>
 @endsection
--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="display-4">{{ __('Edit Category') }}</h1>
-    <a class="btn btn-primary" href="{{ route('category.index') }}">Back</a>
-    <form class="py-5" action="{{route('category.update',$category)}}" method="post">
-        @csrf
-        @method('PUT')
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="title" placeholder="Title" name="title"
-                    value="{{ $category->title }}">
-                <label for="title">Title</label>
+    <div class="container">
+        <h1 class="display-4">{{ __('Edit Category') }}</h1>
+        <a class="btn btn-primary" href="{{ route('category.index') }}">Back</a>
+        <form class="py-5" action="{{route('category.update',$category)}}" method="post">
+            @csrf
+            @method('PUT')
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="title" placeholder="Title" name="title"
+                        value="{{ $category->title }}">
+                    <label for="title">Title</label>
+                </div>
+            @error('title')
+                <div class="form-floating mb-3 text-danger">
+                    {{ $message }}
+                </div>
+            @enderror
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder=" " name="description" >{{ $category->description }}
+                    </textarea>
+                    <label for="description">Description</label>
+                </div>
+            @error('description')
+                <div class="form-floating mb-3 text-danger">
+                    {{ $message }}
+                </div>
+            @enderror
+            <div class="mt-4">
+                <button type="submit" class="btn btn-primary">Save changes</button>
             </div>
-        @error('title')
-            <div class="form-floating mb-3 text-danger">
-                {{ $message }}
-            </div>
-        @enderror
-            <div class="form-floating">
-                <textarea class="form-control" placeholder=" " name="description" >{{ $category->description }}
-                </textarea>
-                <label for="description">Description</label>
-            </div>
-        @error('description')
-            <div class="form-floating mb-3 text-danger">
-                {{ $message }}
-            </div>
-        @enderror
-        <div class="mt-4">
-            <button type="submit" class="btn btn-primary">Save changes</button>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
